3fba1c96e42124ceef4876688ac9aa977699e928
joonhyungbaek <secclint1115@gmail.com>
2024-03-13 11:51:08 +0900

    중복 실행 방지

--- ConnectCheck.cpp
+++ ConnectCheck.cpp
@@ -39,6 +39,15 @@ CConnectCheckApp theApp;
 
 BOOL CConnectCheckApp::InitInstance()
 {
+
+	HWND hWnd = FindWindow(NULL, _T("ConnectCheck"));
+	if (hWnd != NULL)
+	{
+		ShowWindow(hWnd, SW_RESTORE);
+		SetForegroundWindow(hWnd);
+		return TRUE;
+	}
+
 	// 응용 프로그램 매니페스트가 ComCtl32.dll 버전 6 이상을 사용하여 비주얼 스타일을
 	// 사용하도록 지정하는 경우, Windows XP 상에서 반드시 InitCommonControlsEx()가 필요합니다.
 	// InitCommonControlsEx()를 사용하지 않으면 창을 만들 수 없습니다.
